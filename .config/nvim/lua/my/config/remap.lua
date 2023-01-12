46d6c53a7e9331e85bd07a06ea5ce75b52650c5f
Dimitris Gianneler <greyelf1@gmail.com>
2023-01-12 23:04:29 +0200

    Add key bindings for LSP/jdtls

--- .config/nvim/lua/my/config/remap.lua
+++ .config/nvim/lua/my/config/remap.lua
@@ -26,3 +26,22 @@ key("n", "<leader>Y", [["+Y]])
 
 key("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 key("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+
+-- LSP & jdtls
+key("n", "<leader>lh", "<cmd>lua vim.lsp.buf.hover()<CR>", { desc = "LSP: Display documentation [h]elp (e.g. doclet)" })
+key("n", "<leader>lH", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { desc = "LSP: Display function signature [H]elp" })
+key("n", "<leader>ld", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "LSP: Go to [d]efinition" })
+key("n", "<leader>lD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { desc = "LSP: Go to [d]eclaration" })
+key("n", "<leader>lt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { desc = "LSP: Go to [t]ype definition" })
+key("n", "<leader>li", "<cmd>lua vim.lsp.buf.implementation()<CR>", { desc = "LSP: Go to [i]mplementation" })
+key("n", "<leader>ln", "<cmd>lua vim.lsp.buf.rename()<CR>", { desc = "LSP: Re[n]ame symbol" })
+key("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { desc = "LSP: Go to previous diagnostic entry" })
+key("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { desc = "LSP: Go to next diagnostic entry" })
+key("n", "<leader>oi", "<cmd>lua require'jdtls'.organize_imports()<CR>", { desc = "jdtls: [O]rganize [I]mports" })
+key("n", "<leader>rc", "<cmd>lua require'jdtls'.test_class()<CR>", { desc = "jdtls: [R]un test [C]lass" })
+key("n", "<leader>rm", "<cmd>lua require'jdtls'.test_nearest_method()<CR>", { desc = "jdtls: [R]un test [M]ethod" })
+key("n", "<leader>ev", "<cmd>lua require'jdtls'.extract_variable_all()<CR>", { desc = "jdtls: [E]xtract [V]ariable" })
+key("v", "<leader>ev", "<esc><cmd>lua require'jdtls'.extract_variable_all(true)<CR>", { desc = "jdtls: [E]xtract [V]ariable" })
+key("n", "<leader>em", "<cmd>lua require'jdtls'.extract_method()<CR>", { desc = "jdtls: [E]xtract [M]ethod" })
+key("v", "<leader>em", "<esc><cmd>lua require'jdtls'.extract_method(true)<CR>", { desc = "jdtls: [E]xtract [M]ethod" })
